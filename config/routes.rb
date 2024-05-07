b56b8075163f83a8983de7d1e233f01ebf941501
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-05-07 04:16:47 +0000

    photo index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
-
-
   get("/users/:path_username", { :controller => "users", :action => "show"})
+
+
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:photo_id", { :controller => "photos", :action => "show"})
 
 end
