cfe4a94c8c87ee59c6e3deefa2669c6d1942fae6
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-05-07 04:56:06 +0000

    delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,9 +11,28 @@ class PhotosController < ApplicationController
 
 
   def show
+    url_id = params.fetch("photo_id")
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
 
     render({ :template => "photo_templates/show" })
 
   end 
+
+  def delete
+
+    the_id = params.fetch("photo_id")
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/delete" })
+    redirect_to("/photos")
+
+  end
+
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index"})
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
 
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:photo_id", { :controller => "photos", :action => "show"})
+
+  get("delete_photo/:photo_id", { :controller => "photos", :action => "delete"})
 
 end
